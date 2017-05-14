c2a72245939189d3b1ef93150df9f01dd488a3df
yaniv kalfa <netameta1@yahoo.com>
2017-05-14 19:59:23 -0400

    added builtin controllers

--- src/namespaces/presence/actions/userOffline/controllers.js
+++ src/namespaces/presence/actions/userOffline/controllers.js
@@ -1,11 +1,24 @@
-export function inboundController() {
+export function inboundController({ path, client, msg, res, success }) {
+  if ( success ) {
+    let storeToPublish = Options.getStoreToPublish();
+    let msgToPublish = {
+      me: createMe(client, res),
+      payload: message.createMessage({
+        path,
+        payload: {
+          ...createMe(client, res),
+          messageUUID: uuid.v1()
+        }
+      })
+    };
+    storeToPublish.publish(Consts.REDIS_CHANNEL, message.createToPublish(msgToPublish));
+  }
+}
+
+export function publishedController({ path, me, msg, res, success }) {
 
 }
 
-export function publishedController() {
-
-}
-
-export function outboundController() {
+export function outboundController({ path, sendTo, rawMsg }) {
 
 }
--- src/namespaces/presence/actions/userOnline/controllers.js
+++ src/namespaces/presence/actions/userOnline/controllers.js
@@ -1,11 +1,27 @@
-export function inboundController() {
-
+export function inboundController({ path, client, msg, res, success }) {
+  if ( success ) {
+    msg.me = createMe(client, res);
+    let storeToPublish = Options.getStoreToPublish();
+    storeToPublish.publish(Consts.REDIS_CHANNEL, JSON.stringify(msg));
+  }
 }
 
-export function publishedController() {
+export function publishedController({ path, me, msg, res, success }) {
+  let outboundRouter = OutboundRouter();
 
+  if ( success ) {
+    let sendTo = res.sendToResults;
+
+    if ( sendTo && Array.isArray(sendTo) ) {
+      sendTo.forEach((client) => {
+        outboundRouter.route({ path: 'OutboundPresence', sendTo: client, msg });
+      });
+    } else {
+      outboundRouter.route({ path: 'OutboundPresence', sendTo, msg });
+    }
+  }
 }
 
-export function outboundController() {
+export function outboundController({ path, sendTo, rawMsg }) {
 
 }
